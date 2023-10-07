b6fe6b6f205b723d6a43181814ee20df52c02d2c
nqq09 <nqq0915@163.com>
2023-10-07 15:36:35 +0800

    添加了plan_ctrl2的代码

--- plan_ctrl2/scripts/joints_receive_from_arduino.py
+++ plan_ctrl2/scripts/joints_receive_from_arduino.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 import rospy
 from rospy_tutorials.msg import Floats
-from three_dof_planar_manipulator.srv import Floats_array, Floats_arrayResponse, Floats_arrayRequest
+# from three_dof_planar_manipulator.srv import Floats_array, Floats_arrayResponse, Floats_arrayRequest
 pos=0
 vel=0
 
@@ -13,14 +13,14 @@ def my_callback(msg):
 
 def my_server(req):
     global pos, vel
-    res = Floats_arrayResponse() 
-    res.res=[pos, vel]
-    return res
+    # res = Floats_arrayResponse() 
+    # res.res=[pos, vel]
+    return 
 
 rospy.init_node('subscriber_py') #initialzing the node with name "subscriber_py"
 
 rospy.Subscriber("/joint_states_from_arduino", Floats, my_callback, queue_size=10) 
 
-rospy.Service('/read_joint_state', Floats_array, my_server)
+# rospy.Service('/read_joint_state', Floats_array, my_server)
 
 rospy.spin() 
