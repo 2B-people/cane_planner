375dabb72d91a23ed1ca388c382f12912a123984
2B-people <nqq0915@163.com>
2024-05-09 17:30:30 +0800

    <fix>[omni]:testing omni_usb_server, fix bug

---
+++ omniGKF_ctrl/script/test_omni1.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*
+
+import rospy
+from std_msgs.msg import Header
+from omniGKF_control.msg import omniGKFcmd, omniGKFinfo
+import csv
+import matplotlib.pyplot as plt
+import time
+import math
+
+
+# 初始化ROS节点
+rospy.init_node('omni_gkf_controller')
+
+# 创建发布器
+pub = rospy.Publisher('omniGKFcmd', omniGKFcmd, queue_size=10)
+
+# testing
+# 循环发布命令
+delta = 0
+direction = 1
+i = 0
+while not rospy.is_shutdown():
+    cmd = omniGKFcmd()
+    cmd.header = Header(stamp=rospy.Time.now())
+    cmd.gkf_state = True
+    cmd.a = 0.1
+    cmd.varepsilon = 0.1
+    pub.publish(cmd)
+
+    # 等待0.1秒
+    time.sleep(0.1)
+    i = i + 1
+    if i == 20:
+        break
+
+i = 0
+while not rospy.is_shutdown():
+    cmd = omniGKFcmd()
+    cmd.header = Header(stamp=rospy.Time.now())
+    cmd.gkf_state = True
+    cmd.a = 0
+    cmd.varepsilon = 0
+    pub.publish(cmd)
+
+    # 等待0.1秒
+    time.sleep(0.1)
+    i = i + 1
+    if i == 20:
+        break        
+
+# finish testing
+cmd.header = Header(stamp=rospy.Time.now())
+cmd.gkf_state = False
+cmd.a = 0
+cmd.varepsilon = 0 # 将delta从度转换为弧度
+#cmd.delta = 0.0 # 将delta从度转换为弧度
+pub.publish(cmd)
--- omniGKF_ctrl/src/omni_gkf_server.cpp
+++ omniGKF_ctrl/src/omni_gkf_server.cpp
@@ -5,35 +5,61 @@
 
 omni_gkf::OmniGKFUSB usb;
 
+double k1, k2;
+
 void cmdCallback(const omniGKF_control::omniGKFcmd::ConstPtr &msg)
 {
     // 将a和varepsilon转换为float
-    float a = static_cast<float>(msg->a);                   // 假设a的单位是m/s^2
-    float varepsilon = static_cast<float>(msg->varepsilon); // 假设varepsilon的单位是rad/s
-    // int16_t delta = static_cast<int16_t>(msg->delta * 10); // 假设delta的单位是rad
+
     static double last_time = 0;
     static double pos = 0;
     static double vel = 0;
     double current_time = msg->header.stamp.toSec();
-    static double k1 = 6.75 * 19 *60 / (0.09 * 2 * 3.1415926) * 1.2;
-    static double k2 = 8.5 * 19 * 57.3 * 1.2;
-    if (last_time != 0)
-    {
-        double dt = current_time - last_time;
-        pos = pos + varepsilon * dt;
-        //这里的角度pos的单位是rad，需要转换为angle；
-        vel = vel + a * dt;
-        //这里的速度vel的单位是m/s，需要转换成rpm
-        vel = vel * k1;
-        pos = pos * 10 * k2;
-        ROS_WARN("pos: %f, vel: %f", pos, vel);
-        // 发送命令,打印测试的时候把usb相关的注释掉
-        //这里发下去的时候，vel是电机0的转速rpm，pos是电机1的角度angle
-        usb.Set(CMD_VEL, (float)vel, 100); // 设定前进加速度
-        usb.Set(CMD_POS, (float)pos, 100); // 设定转向角速度
-    }
 
-    last_time = current_time;
+    if (msg->gkf_state)
+    {
+        if (msg->a != 0 || msg->varepsilon != 0)
+        {
+            float a = static_cast<float>(msg->a);                   // 假设a的单位是m/s^2
+            float varepsilon = static_cast<float>(msg->varepsilon); // 假设varepsilon的单位是rad/s
+            if (last_time != 0)
+            {
+                double dt = current_time - last_time;
+                pos = pos + varepsilon * dt;
+                // 这里的角度pos的单位是rad，需要转换为angle；
+                vel = vel + a * dt;
+                // 这里的速度vel的单位是m/s，需要转换成rpm
+                double vel_set = vel * k1;
+                double pos_set = pos * k2;
+                ROS_WARN("pos: %f, vel: %f", pos, vel);
+                ROS_WARN("pos_set: %f, vel_set: %f", pos_set, vel_set);
+                // 发送命令,打印测试的时候把usb相关的注释掉
+                // 这里发下去的时候，vel是电机0的转速rpm，pos是电机1的角度angle
+                usb.Set(CMD_VEL, (float)vel_set, 1); // 设定前进加速度
+                usb.Set(CMD_POS, (float)pos_set, 1); // 设定转向角速度
+            }
+        }
+        // debug ,deal with vel and pos
+        if (msg->vel != 0 || msg->pos != 0)
+        {
+            double vel_set = msg->vel * k1;
+            double pos_set = msg->pos * k2;
+            ROS_WARN("pos_set: %f, vel_set: %f", pos_set, vel_set);
+
+            usb.Set(CMD_VEL, (float)vel_set, 1); // 设定前进加速度
+            usb.Set(CMD_POS, (float)pos_set, 1); // 设定转向角速度
+        }
+
+        last_time = current_time;
+    }
+    else
+    {
+        last_time = 0;
+        pos = 0;
+        vel = 0;
+        usb.Set(CMD_VEL, 0);
+        usb.Set(CMD_POS, 0);
+    }
 }
 
 int main(int argc, char **argv)
@@ -46,6 +72,13 @@ int main(int argc, char **argv)
     int usb_baudrate;
     nh.param("omni_gkf_usb_server/port", usb_port, std::string("/dev/ttyACM0"));
     nh.param("omni_gkf_usb_server/baudrate", usb_baudrate, 115200);
+    double k_vel, k_pos;
+    nh.param("gkf_k_vel", k_vel, 1.0);
+    nh.param("gkf_k_pos", k_pos, 1.0);
+
+    // set tranfer param
+    k1 = 6.75 * 19 * 60 / (0.09 * 2 * 3.1415926) * k_vel;
+    k2 = 57.3 * k_pos;
 
     usb.init(usb_port, usb_baudrate); // 使用你的串口和波特率
 
@@ -71,8 +104,8 @@ int main(int argc, char **argv)
             // 测试中代替上面两行usb获取的数据
             //  info.heading = 10;
             //  std::vector<int16_t> vel = {10, 5};
-            //  info.velocity[0] = vel[0];
-            //  info.velocity[1] = vel[1];
+            info.velocity[0] = vel[0];
+            info.velocity[1] = vel[1];
 
             pub.publish(info);
         }
